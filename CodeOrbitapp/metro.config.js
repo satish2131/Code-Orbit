c6283d4a7982fea9e1c54a77fb86c127530238e8
SaiSathishDunna <SaiSathishDunna@flyhii.in>
2026-08-18 21:13:34 +0530

    fix(metro): remove deprecated watcher.unstable_workerThreads option

--- CodeOrbitapp/metro.config.js
+++ CodeOrbitapp/metro.config.js
@@ -3,6 +3,11 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Delete deprecated Metro watcher setting if populated by default Expo config
+if (config.watcher && 'unstable_workerThreads' in config.watcher) {
+  delete config.watcher.unstable_workerThreads;
+}
+
 // Enable Inline Requires to defer module execution until called
 // Reduces initial JavaScript parse & execution time by ~40%, accelerating FCP & TTI
 config.transformer = {
